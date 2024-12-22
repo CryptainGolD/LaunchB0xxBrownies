cde04793c4cb6f309156536f8e055b46c430c02e
Gene <154293262+CryptainGolD@users.noreply.github.com>
2024-12-22 21:54:36 +0900

    Update constants.ts for contract address

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,5 +1,5 @@
 export const NETWORK = import.meta.env.VITE_APP_NETWORK ?? "testnet";
-export const MODULE_ADDRESS = import.meta.env.VITE_MODULE_ADDRESS;
+export const MODULE_ADDRESS = import.meta.env.VITE_MODULE_ADDRESS ?? "0xcfddca9f69cb06c797c91e21ce510a8a252dacd88e3e1777164b9453aa460298";
 export const CREATOR_ADDRESS = import.meta.env.VITE_FA_CREATOR_ADDRESS;
 export const FA_ADDRESS = import.meta.env.VITE_FA_ADDRESS;
 export const IS_DEV = Boolean(import.meta.env.DEV);
